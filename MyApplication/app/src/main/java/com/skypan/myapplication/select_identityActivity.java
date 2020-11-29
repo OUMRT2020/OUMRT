c82f954c05a476313a7f6fdca645ac33b45aef4b
blacksugar890424 <allen.80148@gmail.com>
2020-11-30 00:31:44 +0800

    merge blacksugar and jacky and clark

---
+++ MyApplication/app/src/main/java/com/skypan/myapplication/select_identityActivity.java
@@ -0,0 +1,38 @@
+package com.skypan.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.skypan.myapplication.R;
+
+public class select_identityActivity extends AppCompatActivity {
+    private Button driver_button;
+    private Button passenger_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_identity);
+
+        driver_button=findViewById(R.id.driver_button);
+        driver_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(select_identityActivity.this,DriverMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        passenger_button=findViewById(R.id.passenger_button);
+        passenger_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(select_identityActivity.this,PassengerMainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ MyApplication/app/src/main/java/com/skypan/myapplication/verification_forgetActivity.java
@@ -0,0 +1,51 @@
+package com.skypan.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class verification_forgetActivity extends AppCompatActivity {
+    private TextView cancel_verification_forget;
+    private TextView verify_verification_forget;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_forget);
+
+        // find id
+        cancel_verification_forget=findViewById(R.id.cancel_verification_forget);
+        verify_verification_forget=findViewById(R.id.verify_verification_forget);
+
+        // 監聽器
+        setListeners();
+    }
+
+    // 監聽器
+    private void setListeners(){
+        OnClick onClick=new OnClick();
+        cancel_verification_forget.setOnClickListener(onClick);
+        verify_verification_forget.setOnClickListener(onClick);
+    }
+
+    // when click ...
+    private class OnClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent intent=null;
+            switch (v.getId()){
+                case R.id.cancel_verification_forget:
+                    // 跳轉到輸入驗證信箱(忘記密碼的)畫面
+                    intent=new Intent(verification_forgetActivity.this, forget_password.class);
+                    break;
+                case R.id.verify_verification_forget:
+                    // 跳轉到設定新密碼頁面
+                    intent=new Intent(verification_forgetActivity.this, set_new_passwordActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    }
+}
