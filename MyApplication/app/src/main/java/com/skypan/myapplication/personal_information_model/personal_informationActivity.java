c77173db55d36b116a7ac00673525d0c80dc5d67
blacksugar890424 <allen.80148@gmail.com>
2020-12-03 20:53:21 +0800

    add check_logoutActivity, personal_informationActivity

---
+++ MyApplication/app/src/main/java/com/skypan/myapplication/personal_information_model/personal_informationActivity.java
@@ -0,0 +1,14 @@
+package com.skypan.myapplication.personal_information_model;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.skypan.myapplication.R;
+
+public class personal_informationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
