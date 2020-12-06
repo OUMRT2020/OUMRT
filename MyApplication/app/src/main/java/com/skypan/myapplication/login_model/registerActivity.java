c06354a340e0d3d07e5ea3d869c0b316651c183a
blacksugar890424 <allen.80148@gmail.com>
2020-12-06 22:25:47 +0800

    Update registerActivity.java

--- MyApplication/app/src/main/java/com/skypan/myapplication/login_model/registerActivity.java
+++ MyApplication/app/src/main/java/com/skypan/myapplication/login_model/registerActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.skypan.myapplication.R;
 
 public class registerActivity extends AppCompatActivity {
