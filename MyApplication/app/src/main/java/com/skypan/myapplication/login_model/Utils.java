9b6e3ddfc1e7f7daf1b4ad8d21c36fe2b1c275e6
blacksugar890424 <allen.80148@gmail.com>
2020-12-12 12:46:17 +0800

    can send verify number without compare

---
+++ MyApplication/app/src/main/java/com/skypan/myapplication/login_model/Utils.java
@@ -0,0 +1,6 @@
+package com.skypan.myapplication.login_model;
+
+public class Utils {
+    public static final String EMAIL = "allen.80148@gmail.com";
+    public static final String PASSWORD = "allen60110";
+}
--- MyApplication/app/src/main/java/com/skypan/myapplication/login_model/registerActivity.java
+++ MyApplication/app/src/main/java/com/skypan/myapplication/login_model/registerActivity.java
@@ -4,14 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.skypan.myapplication.R;
+import java.util.Random;
 
 public class registerActivity extends AppCompatActivity {
     private Button cancel_register;
     private Button verify_register;
+    public EditText email_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class registerActivity extends AppCompatActivity {
         // find id
         cancel_register = findViewById(R.id.cancel_register);
         verify_register = findViewById(R.id.verify_register);
+        email_register = findViewById(R.id.email_register);
 
         // 監聽器
         setListeners();
@@ -45,10 +47,39 @@ public class registerActivity extends AppCompatActivity {
                     break;
                 case R.id.verify_register:
                     // 跳轉到認證信箱(註冊的)頁面
+                    sendMail();
                     intent = new Intent(registerActivity.this, verification_register.class);
                     break;
             }
             startActivity(intent);
         }
+    }
+
+    private void sendMail() {
+        // System.out.println("XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+
+        // declare userMail && mailSubject
+        String mail = email_register.getText().toString().trim();
+        String subject = "OUMRT_verify";
+
+        // produce verify number
+        String str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random random = new Random();
+        StringBuffer stringBuffer = new StringBuffer();
+
+        // 設定字串長度為 4
+        int strLength = 4;
+        for (int j = 0; j < strLength; j++) {
+            int index = random.nextInt(str.length());
+            char c = str.charAt(index);
+            stringBuffer.append(c);
+        }
+
+        //將StringBuffer轉換為String型別的字串
+        String message = stringBuffer.toString();
+
+        // send mail
+        JavaMailAPI javaMailAPI=new JavaMailAPI(this,mail,subject,message);
+        javaMailAPI.execute();
     }
 }
