adb5b0ebb0532bd4ba1d559de0e5942644da0921
Clark <49876290+CLARKCLARKCLARK@users.noreply.github.com>
2021-01-08 00:07:59 +0800

    history詳細

--- MyApplication/app/src/main/java/com/skypan/myapplication/driver_model/ui/history/SearchedHistoryAdapter.java
+++ MyApplication/app/src/main/java/com/skypan/myapplication/driver_model/ui/history/SearchedHistoryAdapter.java
@@ -51,10 +51,14 @@ public class SearchedHistoryAdapter extends RecyclerView.Adapter<SearchedHistory
     @Override
     public void onBindViewHolder(@NonNull SearchedHistoryAdapter.ViewHolder holder, int position) {
 
-        holder.event_name.setText("" + Events.get(position).getEvent_name());
-        holder.event_time.setText("" + Events.get(position).getAcceptable_time_interval().get(0).toString()
-                + " 至 " + Events.get(position).getAcceptable_time_interval().get(1).toString());
-        holder.event_cost.setText("" + Events.get(position).getPrice());
+        holder.event_name.setText(Events.get(position).getEvent_name());
+        holder.event_time.setText(Events.get(position).getMy_request().getActual_time().toString());
+        holder.event_cost.setText(Events.get(position).getPrice());
+        if (Events.get(position).getStatus().equals("red")) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#FF0000"));
+        } else if (Events.get(position).getStatus().equals("gray")) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#808080"));
+        }
     }
 
     @Override
@@ -65,7 +69,7 @@ public class SearchedHistoryAdapter extends RecyclerView.Adapter<SearchedHistory
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView event_name, event_time, event_time2, event_cost;
-        private Button event_delete;
+        private Button event_rate;
         ;
 
         public ViewHolder(@NonNull final View itemView) {
@@ -74,7 +78,62 @@ public class SearchedHistoryAdapter extends RecyclerView.Adapter<SearchedHistory
             event_name = itemView.findViewById(R.id.event_title);
             event_time = itemView.findViewById(R.id.event_time);
             event_cost = itemView.findViewById(R.id.event_cost);
-            event_delete = itemView.findViewById(R.id.event_delete);
+            event_rate = itemView.findViewById(R.id.event_rate);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    final int position = getAdapterPosition();
+                    final Past_Event e = Events.get(position);
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+
+                    final View content_layout = LayoutInflater.from(mContext).inflate(R.layout.driver_main_event_detail, null);
+
+                    TextView foo;
+                    foo = content_layout.findViewById(R.id.tv_1);
+                    foo.setText("姓名");
+                    foo = content_layout.findViewById(R.id.driver_name);//直接用乘客主畫面的layout了 id不想改
+                    foo.setText(e.getUser().getName());
+
+                    foo = content_layout.findViewById(R.id.tv_2);
+                    foo.setText("性別");
+                    foo = content_layout.findViewById(R.id.driver_sex);
+                    foo.setText(e.getUser().isSex() ? "男" : "女");
+
+                    foo = content_layout.findViewById(R.id.tv_3);//顯示乘客的電話
+                    foo.setVisibility(View.VISIBLE);
+                    foo = content_layout.findViewById(R.id.driver_phone);
+                    foo.setVisibility(View.VISIBLE);
+
+                    foo = content_layout.findViewById(R.id.actual_start_point);
+                    foo.setText(e.getFinal_request().getActual_start_point());
+
+                    foo = content_layout.findViewById(R.id.actual_end_point);
+                    foo.setText(e.getFinal_request().getActual_end_point());
+
+                    foo = content_layout.findViewById(R.id.actual_time);
+                    foo.setText(e.getFinal_request().getActual_time());
+
+
+                    foo = content_layout.findViewById(R.id.tv_extra_need);
+                    foo.setText(e.getFinal_request().getExtra_needed());
+
+                    alertDialog.setPositiveButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+
+
+                    alertDialog.setView(content_layout);
+                    alertDialog.show();
+                }
+            });
+
+
 
         }
     }
